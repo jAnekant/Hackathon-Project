87c8c4da59b3ecf86465edaf7092dbdd747903c4
jAnekant <51898340+jAnekant@users.noreply.github.com>
2021-01-18 14:02:07 +0530

    I am the boss

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginPageView, RegisterPageView,MainPageView
+from .views import LoginPageView, RegisterPageView,MainPageView,Login_view
 
 urlpatterns = [
     path('', MainPageView.as_view(),name='mainpage'),
+    path('register/', Login_view),
     path('login/', LoginPageView.as_view(), name='login'),
     path('register/', RegisterPageView.as_view(), name='register'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from .models import Login
 
 class LoginPageView(TemplateView):
     template_name = 'login.html'
@@ -9,3 +11,19 @@ class RegisterPageView(TemplateView):
 
 class MainPageView(TemplateView):
     template_name = 'mainpage.html'
+
+def Login_view(request):
+    if request.method == 'POST':
+        record = Login()
+        record.email = request.POST.get('email')
+        record.pwd   = request.POST.get('password')
+        record.login   = request.POST.get('login')
+        record.state   = request.POST.get('state')
+        record.city   = request.POST.get('city')
+        record.address   = request.POST.get('address')
+        print(record.email)
+        print(record.pwd)
+        record.save()
+        return render(request, 'register.html')
+    else:
+        return render(request, 'register.html')
